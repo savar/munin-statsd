32246e8b2c5d3d929f6685a3d1c19b29eaa6ed12
Simon Effenberg <savar@schuldeigen.de>
2013-05-16 17:38:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+munin-statsd
+============
+A Ruby script to send Munin-node data to a StatsD instance. As written, this is designed to be run from cron.
+
+This was inspired by Tech-Corps/munin-statsd
+
+Requirements
+============
+
+* sosedoff/munin-ruby > 0.2.4
+* github/statsd-ruby maybe reinh/statsd
+
+Configuration
+=============
+Set the ENV variables `SCHEMABASE`, `STATSD_HOST` and `STATSD_PORT` as appropriate for your environment (see in the code for default values).
+If you're not running the script on the same host like the munin-node is running than set `MUNIN_HOST` and `MUNIN_PORT` as well.
+
+Installation
+============
+1. Clone the repository
+```
+    cd /usr/local/bin
+    git clone https://github.com/Savar/munin-statsd.git
+```
+
+2. Make sure munin-statsd.rb is executable
+```
+    chmod +x munin-statsd/munin-statsd.rb
+```
+
+3. Create a crontab entry that calls the script
+```
+    * * * * * SCHEMABASE=my_cool.base.schema STATSD_HOST=something.somewhere /usr/local/bin/munin-statsd/munin-statsd.rb
+```
---
+++ munin-statsd.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'statsd'
+require 'munin-ruby'
+
+MUNIN_HOST = ENV['MUNIN_HOST'] || "localhost"
+MUNIN_PORT = ENV['MUNIN_PORT'] || 4949
+
+STATSD_HOST = ENV['STATSD_HOST'] || "localhost"
+STATSD_PORT = ENV['STATSD_PORT'] || 8125
+
+SCHEMABASE = ENV['SCHEMABASE'] || "munin_statsd"
+
+node = Munin::Node.new(MUNIN_HOST, MUNIN_PORT)
+statsd = Statsd.new(STATSD_HOST, STATSD_PORT)
+statsd.namespace = SCHEMABASE
+
+def statsd_method(config, name)
+  type = config["metrics"][name]["type"]
+  case type
+  when "DERIVE", "COUNTER", "ABSOLUTE"
+    return :count
+  when "GAUGE", nil
+    return :gauge
+  else
+    STDERR.puts "WARNING: unknown munin type #{type} .. using GAUGE instead"
+    return :gauge
+  end
+end
+
+services = node.list
+STDOUT.puts "collection stuff for services #{services.inspect}"
+STDOUT.puts "collection configs"
+configs = node.config services
+STDOUT.puts "collection data"
+all_data = node.fetch services
+services.each do |service|
+  config = configs[service]
+  data   = all_data[service]
+
+  next if data.nil?
+
+  data.each_pair do |name, value|
+    statsd.send(statsd_method(config, name), "#{config["graph"]["category"]}.#{service}.#{name}", value)
+  end
+end
